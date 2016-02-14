41b494faf7618ca26d0225025efb6652216f7d12
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-14 13:25:32 -0500

    adding variables and pseudocode for js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,27 @@
+//pseudocode:
+// for testing, add an alert event listener to check if targeting correct element
 
+// declare variables for all input elements and use input event
+// declare variables for all select (drop-down) elements and use change event
+// declare variables for all textarea elemeents and use input event
+// declare variable for the send your data button and use submit event
+
+var firstName = document.getElementById("first-name");
+var lastName = document.getElementById("last-name");
+var email = document.getElementById("email");
+var phone = document.getElementById("number");
+var pet = document.getElementById("dog");
+var gadget = document.getElementById("gadget");
+var favDrink = document.getElementById("drink");
+var superPower = document.getElementById("power");
+var weapon = document.getElementById("weapon");
+
+var country = document.getElementById("countries");
+var job = document.getElementById("occupations");
+var talent = document.getElementById("talant");
+
+var biography = document.getElementById("bio");
+var affil = document.getElementById("affiliations");
+var comment = document.getElementById("comments");
+
+var submitData = document.getElementbyId("submit-form");
