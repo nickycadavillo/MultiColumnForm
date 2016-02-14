351e066549b4bacff3cff1b1d8563e31ffcc1462
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-14 14:39:10 -0500

    adding comments for reference

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,78 +28,78 @@ var submitData = document.getElementById("submit-form");
 
 firstName.addEventListener('input', function(prevDefault) {
   //alert("Hi");
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(firstName.value);
 });
 
 lastName.addEventListener('input', function(prevDefault) {
   //alert("Hi");
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(lastName.value);
 });
 
 email.addEventListener('input', function(prevDefault) {
   //alert("Hi");
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(email.value);
 });
 
 phone.addEventListener('input', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(phone.value);
 });
 
 pet.addEventListener('input', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(pet.value);
 });
 
 gadget.addEventListener('input', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(gadget.value);
 });
 
 favDrink.addEventListener('input', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(favDrink.value);
 });
 
 superPower.addEventListener('input', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(superPower.value);
 });
 
 weapon.addEventListener('input', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(weapon.value);
 });
 
 bio.addEventListener('input', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(bio.value);
 });
 
 affil.addEventListener('input', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(affil.value);
 });
 
 comment.addEventListener('input', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(comment.value);
 });
 
 country.addEventListener('change', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(country.value);
 });
 
 job.addEventListener('change', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(job.value);
 });
 
 talent.addEventListener('change', function(prevDefault) {
-  prevDefault.preventDefault();
+  prevDefault.preventDefault(); //don't submit form yet
   console.log(talent.value);
 });
